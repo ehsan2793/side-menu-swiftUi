00b47313f22eebb97d0a96d711ed02c12774a180
ehsan2793 <ehsan2793@gmail.com>
2024-03-23 11:42:59 -0700

    Initial Commit

---
+++ side menu/side_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  side_menuApp.swift
+//  side menu
+//
+//  Created by Ehsan Rahimi on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct side_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
