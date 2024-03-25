81ad2b65071262df925ca99c0ebd80d9f522db17
ehsan2793 <ehsan2793@gmail.com>
2024-03-24 22:15:05 -0700

    done

--- side menu/SideMenuView.swift
+++ side menu/SideMenuView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SideMenuView: View {
     @Binding var isShowing: Bool
+    @Binding var selectedTab: Int
     @State private var selectedOption: SideMenuOptionModel?
 
     var body: some View {
@@ -23,33 +24,39 @@ struct SideMenuView: View {
                 HStack {
                     VStack(alignment: .leading, spacing: 32) {
                         SideMenuHeaderView()
-                        
+
                         VStack {
                             ForEach(SideMenuOptionModel.allCases) { option in
                                 Button {
-                                    selectedOption = option
+                                    onOptionTapped(option: option)
                                 } label: {
                                     SideMenuRowView(option: option, selectedOption: $selectedOption)
                                 }
-
                             }
                         }
-                        
+
                         Spacer()
                     }
                     .padding()
-                    .frame(width: 270,alignment: .leading)
+                    .frame(width: 270, alignment: .leading)
                     .background(.white)
 
                     Spacer()
                 }
+                .transition(.move(edge: .leading))
             }
         }
-        .transition(.move(edge: .leading))
+
         .animation(.easeInOut, value: isShowing)
+    }
+
+    private func onOptionTapped(option: SideMenuOptionModel) {
+        selectedOption = option
+        selectedTab = option.rawValue
+        isShowing = false
     }
 }
 
 #Preview {
-    SideMenuView(isShowing: .constant(true))
+    SideMenuView(isShowing: .constant(true), selectedTab: .constant(1))
 }
